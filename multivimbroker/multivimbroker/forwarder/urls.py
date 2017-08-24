a9562c1a7a41f0da36779193811f79fba2974e9b
Ethan Lynn <ethanlynnl@vmware.com>
2017-08-24 11:46:12 +0800

    Add registry API to MultiCloud framework
    issue-id: MULTICLOUD-59
    Change-Id: I0e5323c49cc93c940ce1a8878a8c14e5d76b2e4c
    Signed-off-by: Ethan Lynn <ethanlynnl@vmware.com>

--- multivimbroker/multivimbroker/forwarder/urls.py
+++ multivimbroker/multivimbroker/forwarder/urls.py
@@ -17,13 +17,18 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from multivimbroker.forwarder.views import Forward
 from multivimbroker.forwarder.views import Identity
+from multivimbroker.forwarder.views import Registry
 
 
 urlpatterns = [
     url(r'^api/multicloud/v0/(?P<vimid>[0-9a-zA-Z_-]+)/identity/v3$',
         Identity.as_view()),
-    url(r'^api/multicloud/v0/(?P<vimid>[0-9a-zA-Z_-]+)/\
-        identity/v3/auth/tokens$', Identity.as_view()),
+    url(r'^api/multicloud/v0/(?P<vimid>[0-9a-zA-Z_-]+)/identity/v3'
+        r'/auth/tokens$', Identity.as_view()),
+    url(r'^api/multicloud/v0/(?P<vimid>[0-9a-zA-Z_-]+)/registry$',
+        Registry.as_view()),
+    url(r'^api/multicloud/v0/(?P<vimid>[0-9a-zA-Z_-]+)$',
+        Registry.as_view()),
     url(r'^api/multicloud/v0/(?P<vimid>[0-9a-zA-Z_-]+)',
         Forward.as_view()),
 ]
