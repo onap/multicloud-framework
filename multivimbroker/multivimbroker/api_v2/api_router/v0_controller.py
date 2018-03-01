fcfb692a6927c8e8c39368ed2717da9f75f038c0
Hong Hui Xiao <honghui_xiao@yeah.net>
2018-03-01 16:25:40 +0800

    Add forwarder function to pecan framework
    Add forward(or proxy) functionality, so that multicloud component
    is able to forward request to another component.
    
    TEST DONE:
    GET POST DELETE (/networks)
    
    Change-Id: I73f71c62a91301c0864f1a00fd95f016f69a89d4
    Issue-ID: MULTICLOUD-150
    Signed-off-by: Hong Hui Xiao <honghui_xiao@yeah.net>

--- multivimbroker/multivimbroker/api_v2/api_router/v0_controller.py
+++ multivimbroker/multivimbroker/api_v2/api_router/v0_controller.py
@@ -10,9 +10,16 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+import logging
 import pecan
 
 from multivimbroker.swagger import utils
+from multivimbroker.pub import exceptions
+from multivimbroker.pub.utils import restcall
+from multivimbroker.pub.utils import syscomm
+
+
+logger = logging.getLogger(__name__)
 
 
 class V0_Controller(object):
@@ -20,3 +27,38 @@ class V0_Controller(object):
     @pecan.expose('json', route="swagger.json")
     def swagger_json(self):
         return utils.get_swagger_json_data()
+
+    @pecan.expose()
+    def _route(self, remainder, request):
+        return self.forwarder, remainder
+
+    @pecan.expose('json')
+    def forwarder(self, *remainder, **kwargs):
+        """ Forward any requests that don't have a specific match """
+
+        # TODO(xiaohhui): Add validator for vim_id.
+        vim_id = remainder[0]
+        request = pecan.request
+        try:
+            vim_url = syscomm.getMultivimDriver(vim_id,
+                                                full_path=request.path)
+
+            # NOTE: Not sure headers should be set here. According to original
+            # code, headers are discarded.
+            retcode, content, status_code, resp = restcall.req_by_msb(
+                vim_url, request.method, content=request.body)
+        except exceptions.NotFound as e:
+            pecan.abort(404, detail=str(e))
+        except Exception as e:
+            pecan.abort(500, detail=str(e))
+
+        if retcode:
+            # Execptions are handled within req_by_msb
+            logger.error("Status code is %s, detail is %s.",
+                         status_code, content)
+        response = pecan.Response(body=content, status=status_code)
+
+        for k in syscomm.getHeadersKeys(resp):
+            response.headers[k] = resp[k]
+
+        return response
