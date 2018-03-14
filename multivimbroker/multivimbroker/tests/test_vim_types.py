f4b45ea1c3a00aec61d75d63b5c8bab24bc9646b
Ethan Lynn <ethanlynnl@vmware.com>
2018-03-14 16:11:00 +0800

    Add test_vim_types_success
    Add unit tests for vim_types API.
    
    Change-Id: I188f7a65f6573ae3fb5813a43c34245eebb2f4fa
    Issue-ID: MULTICLOUD-184
    Signed-off-by: Ethan Lynn <ethanlynnl@vmware.com>

---
+++ multivimbroker/multivimbroker/tests/test_vim_types.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2017-2018 VMware, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import mock
+import unittest
+
+from rest_framework.views import status
+
+from multivimbroker.forwarder.views import VIMTypes
+
+
+class TestUrls(unittest.TestCase):
+    def setUp(self):
+        self.view = VIMTypes()
+
+    def test_vim_types_success(self):
+        resp = self.view.get(mock.Mock())
+        expect_body = [
+            {
+                'vim_type': 'openstack',
+                'versions': ['ocata', 'titanium_cloud']
+            },
+            {
+                'vim_type': 'vmware',
+                'versions': ['4.0']
+            }
+        ]
+        self.assertEqual(status.HTTP_200_OK, resp.status_code)
+        self.assertEqual(expect_body, resp.data)
