7419e38021a3c959af3b6a4730a0f50baff2e15b
Bin Yang <bin.yang@windriver.com>
2018-08-16 06:49:12 +0000

    API upgrading for forwarder API
    Change-Id: Iabbab0fd3cd52f728fe69599110b0168c48fb17a
    Issue-ID: MULTICLOUD-310
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- multivimbroker/multivimbroker/forwarder/urls.py
+++ multivimbroker/multivimbroker/forwarder/urls.py
@@ -25,6 +25,15 @@ from multivimbroker.forwarder.views import UnRegistry
 from multivimbroker.forwarder.views import VIMTypes
 from multivimbroker.forwarder.views import MultiPartView
 
+# API v1
+from multivimbroker.forwarder.views import APIv1CheckCapacity
+from multivimbroker.forwarder.views import APIv1Extension
+from multivimbroker.forwarder.views import APIv1Forward
+from multivimbroker.forwarder.views import APIv1Identity
+from multivimbroker.forwarder.views import APIv1Registry
+from multivimbroker.forwarder.views import APIv1UnRegistry
+from multivimbroker.forwarder.views import APIv1VIMTypes
+from multivimbroker.forwarder.views import APIv1MultiPartView
 
 urlpatterns = [
     url(r'^api/multicloud/v0/vim_types$',
@@ -45,6 +54,33 @@ urlpatterns = [
         MultiPartView.as_view()),
     url(r'^api/multicloud/v0/(?P<vimid>[0-9a-zA-Z_-]+)',
         Forward.as_view()),
+
+    # API v1
+    url(r'^api/multicloud/v1/vim_types$',
+        APIv1VIMTypes.as_view()),
+    url(r'^api/multicloud/v1/check_vim_capacity$',
+        APIv1CheckCapacity.as_view()),
+    url(r'^api/multicloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)'
+        r'/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/identity/v3$',
+        APIv1Identity.as_view()),
+    url(r'^api/multicloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)'
+        r'/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/identity/v3'
+        r'/auth/tokens$', APIv1Identity.as_view()),
+    url(r'^api/multicloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)'
+        r'/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/registry$',
+        APIv1Registry.as_view()),
+    url(r'^api/multicloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)i'
+        r'/(?P<cloud_region_id>[0-9a-zA-Z_-]+)$',
+        APIv1UnRegistry.as_view()),
+    url(r'^api/multicloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)'
+        r'/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/extensions$',
+        APIv1Extension.as_view()),
+    url(r'^api/multicloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)'
+        r'/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/multipart',
+        APIv1MultiPartView.as_view()),
+    url(r'^api/multicloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)'
+        r'/(?P<cloud_region_id>[0-9a-zA-Z_-]+)',
+        APIv1Forward.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
