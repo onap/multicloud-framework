82a451d019afd804d5eea61ebe8d6b1814a065fc
liboNet <libo.zhu@intel.com>
2019-03-29 07:29:51 +0800

    Add model to represent VfModuleModel and Artifact management
    . add VfModuleModel to represent content of VfModule
    . add Gsonutil to support List which is the content of Vf
      Module meta json
    . add CloudArtifact to deal with Artifact for subplug action
    . Fix pom.xml typo
    
    Change-Id: Ic459118323bf50939493b0f0feff7ba96dd9a64d
    Issue-ID: MULTICLOUD-511
    Signed-off-by: liboNet <libo.zhu@intel.com>

---
+++ artifactbroker/model/src/main/java/org/onap/policy/distribution/model/VfModuelModel.java
@@ -0,0 +1,62 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.distribution.model;
+
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Represents a VfModuleModel that a {@link VfModuleModel} can be parsed.
+ */
+
+public class VfModuelModel {
+
+    private String vfModuleModelName;
+    private String vfModuleModelInvariantUUID;
+    private String vfModuleModelVersion;
+    private String vfModuleModelUUID;
+    private String vfModuleModelCustomizationUUID;
+    private String vfModuleModelDescription;
+    private Boolean isBase;
+    private List<String> artifacts;
+    private Map<String, Object> properties;
+
+    public String getVfModuleModelName() {
+        return vfModuleModelName;
+    }
+
+    public String getVfModuleModelCustomizationUUID() {
+        return vfModuleModelCustomizationUUID;
+    }
+
+    public String getVfModuleModelDescription() {
+        return vfModuleModelDescription;
+    }
+
+    public List<String> getArtifacts() {
+        return artifacts;
+    }
+
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+}
