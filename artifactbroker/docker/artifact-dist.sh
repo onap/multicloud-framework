76cb15908ebfab78034aff381ba5c867b5491c05
liboNet <libo.zhu@intel.com>
2019-04-12 02:41:04 +0800

    add support to generate docker image for artifactbroker
    . add Dockerfile to describe the docker image steps
    . add build_image.sh to wrap the generation of image
    . Use Alpine as base docker image in distribution to make
      it small
    
    Change-Id: I8a2c8321be176d549063e6ab296adcacb46b9ad3
    Issue-ID: MULTICLOUD-548
    Signed-off-by: liboNet <libo.zhu@intel.com>

---
+++ artifactbroker/docker/artifact-dist.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# ============LICENSE_START=======================================================
+#  Copyright (C) 2018 Ericsson. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
+#
+
+JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk/
+ARTIFACT_DISTRIBUTION_HOME=/opt/app/distribution
+KEYSTORE="${ARTIFACT_HOME}/etc/ssl/policy-keystore"
+KEYSTORE_PASSWD="Pol1cy_0nap"
+TRUSTSTORE="${ARTIFACT_HOME}/etc/ssl/policy-truststore"
+TRUSTSTORE_PASSWD="Pol1cy_0nap"
+
+
+if [ "$#" -eq 1 ]; then
+    CONFIG_FILE=$1
+else
+    CONFIG_FILE=${CONFIG_FILE}
+fi
+
+if [ -z "$CONFIG_FILE" ]
+  then
+    CONFIG_FILE="$ARTIFACT_DISTRIBUTION_HOME/etc/defaultConfig.json"
+fi
+
+echo "Policy distribution config file: $CONFIG_FILE"
+
+$JAVA_HOME/bin/java -cp "$ARTIFACT_DISTRIBUTION_HOME/etc:$ARTIFACT_DISTRIBUTION_HOME/lib/*" -Djavax.net.ssl.keyStore="$KEYSTORE" -Djavax.net.ssl.keyStorePassword="$KEYSTORE_PASSWD" -Djavax.net.ssl.trustStore="$TRUSTSTORE" -Djavax.net.ssl.trustStorePassword="$TRUSTSTORE_PASSWD" org.onap.policy.distribution.main.startstop.Main -c $CONFIG_FILE
