49c352b7fb5acfecf9b882dd5cd2a08345733324
Bin Yang <bin.yang@windriver.com>
2021-08-19 13:17:26 +0800

    Remove dependency to onaplog
    due to it does not support python3.8
    
    Issue-ID: MULTICLOUD-1382
    
    Signed-off-by: Bin Yang <bin.yang@windriver.com>
    Change-Id: I31441761b9742e73781aac5f9d128f61a339b952

--- multivimbroker/multivimbroker/settings.py
+++ multivimbroker/multivimbroker/settings.py
@@ -12,9 +12,9 @@
 
 import os
 import sys
-from logging import config
-from onaplogging import monkey
-monkey.patch_all()
+import platform
+import yaml
+from logging import config as log_config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -51,7 +51,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'multivimbroker.middleware.LogContextMiddleware',
 ]
 
 ROOT_URLCONF = 'multivimbroker.urls'
@@ -82,15 +81,51 @@ TIME_ZONE = 'UTC'
 
 STATIC_URL = '/static/'
 
+if platform.system() == 'Windows' or 'test' in sys.argv:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s:[%(name)s]:[%(filename)s]'
+                +'-[%(lineno)d] [%(levelname)s]:%(message)s',
+            },
+        },
+        'filters': {
+        },
+        'handlers': {
+            'file_handler': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs/test.log'),
+                'formatter': 'standard',
+                'maxBytes': 1024 * 1024 * 50,
+                'backupCount': 5,
+            },
+        },
 
-LOGGING_CONFIG = None
-# yaml configuration of logging
-LOGGING_FILE = os.path.join(BASE_DIR, 'multivimbroker/pub/config/log.yml')
-config.yamlConfig(filepath=LOGGING_FILE, watchDog=True)
+        'loggers': {
+            'common': {
+                'handlers': ['file_handler'],
+                'level': 'DEBUG',
+                'propagate': False
+            },
+        }
+    }
+else:
+    log_path = "/var/log/onap/multicloud/multivimbroker"
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
 
+    LOGGING_CONFIG = None
+    # yaml configuration of logging
+    LOGGING_FILE = os.path.join(BASE_DIR, 'multivimbroker/pub/config/log.yml')
+    with open(file=LOGGING_FILE, mode='r', encoding="utf-8")as file:
+        logging_yaml = yaml.load(stream=file, Loader=yaml.FullLoader)
+    log_config.dictConfig(config=logging_yaml)
 
 if 'test' in sys.argv:
-    from multivimbroker.pub.config import config
+    # from multivimbroker.pub.config import config
     REST_FRAMEWORK = {}
     import platform
 
